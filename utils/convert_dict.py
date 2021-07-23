91f54b259c140dda9e4f303088fac566a39cf7b0
风起 <37897216+wikiZ@users.noreply.github.com>
2021-07-23 11:46:09 +0800

    工具类

---
+++ utils/convert_dict.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: 风起
+@contact: onlyzaliks@gmail.com
+@File: convert_dict.py
+@Time: 2021/7/3 0:47
+'''
+
+
+class Dict(dict):
+    __setattr__ = dict.__setitem__
+    __getattr__ = dict.__getitem__
+
+
+def convert(dictObj):
+    if not isinstance(dictObj, dict):
+        return dictObj
+    json = Dict()
+    for k, v in dictObj.items():
+        json[k] = convert(v)
+    return json
